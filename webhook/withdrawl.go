d8aa30ca970117ea1b003d06e167491c3daffbe0
Jesse Federkiewicz <JFed@zera.vision>
2025-02-27 10:30:34 -0700

    added template for deposit / withdrawl processing with txn data passed to func

---
+++ webhook/withdrawl.go
@@ -0,0 +1,12 @@
+package webhook
+
+import (
+	"github.com/ZeraVision/zn-wallet-manager/database"
+)
+
+// *** Example processing of a deposit, modify for your specific use case ***
+func withdrawl(db database.Database, txn TransactionInfo) error {
+	// Process the deposit how you see fit (updating a database, sending a notification, etc.)
+
+	return nil
+}
